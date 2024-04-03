5c91c67438388fb51b92acbfe05c48045717f9da
Amal Penahli <amalpenahli@Amals-MacBook-Air.local>
2024-04-03 21:41:52 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       Scanner  ay = new Scanner(System.in);
+
+       String a = ay.nextLine();
+
+       switch (a){
+           case "yanvar":
+               System.out.println(a+" ayi 31 gun");
+               break;
+
+           case "fevral":
+               System.out.println(a+ " ayi 28 gun");
+               break;
+
+           case "mart":
+               System.out.println(a+ " ayi 31 gun");
+               break;
+
+           case "aprel":
+               System.out.println(a+ " ayi 30 gun");
+               break;
+
+           case "may":
+               System.out.println(a+ " ayi 31 gun");
+               break;
+
+           case "iyun":
+               System.out.println(a+ " ayi 30 gun");
+               break;
+
+           case "iyul":
+               System.out.println(a+ " ayi 31 gun");
+               break;
+
+           case "avqust":
+               System.out.println(a+ " ayi 31 gun");
+               break;
+
+           case "sentyabr":
+               System.out.println(a+ " ayi 30 gun");
+               break;
+
+           case "oktyabr":
+               System.out.println(a+ " ayi 31 gun");
+               break;
+
+           case "noyabr":
+               System.out.println(a+ " ayi 30 gun");
+               break;
+
+           case "dekabr":
+               System.out.println(a+ " ayi 31 gun");
+               break;
+           default:
+
+               System.out.println("bele ay yoxdur");
+
+       }
+
+
+
+
+
+
+
+
+
+
+
+     }
+
+
+
+
+    }
